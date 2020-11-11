4393b406e65cd48872aeae3af915f888a536372f
Massimo Darmanin <Massimo.Darmanin.c10229@mcast.edu.mt>
2020-11-11 19:33:30 +0100

    Creating the basis for a clean architecture

--- SWD62AEP/Presentation/Controllers/SupportController.cs
+++ SWD62AEP/Presentation/Controllers/SupportController.cs
@@ -8,8 +8,30 @@ namespace Presentation.Controllers
 {
     public class SupportController : Controller
     {
+        [HttpGet]
         public IActionResult Contact()
         {
+            return View();
+        }
+
+
+        [HttpPost]
+         public IActionResult Contact(string email, string query)
+        {
+            //ViewData, ViewBag
+
+            if (string.IsNullOrEmpty(query))
+            {
+                ViewData["Feedback"] = "Question was left empty";
+                //ViewData["divcolor"] = "warning";
+            }
+            else
+            {
+                ViewData["FeedBack"] = "Thankyou for your query, we will get back to you asap";
+                //ViewBag.feedback = "Thankyou for your query, we will get back to you asap";
+                //ViewData["divcolor"] = "normal";
+            }
+
             return View();
         }
     }
